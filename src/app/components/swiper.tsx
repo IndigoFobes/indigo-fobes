fe65ca95880ba76d8b5e1a6fd576a2cf537deb2e
Indigo <indigo.crandell@gmail.com>
2024-03-08 15:50:44 -0500

    Took nav arrows off of swiper for now. Will hopefully just add my own later.

--- src/app/components/swiper.tsx
+++ src/app/components/swiper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
@@ -29,7 +29,6 @@ export default function ImageSlideshow(props: Props) {
       <Swiper
         loop={true}
         spaceBetween={10}
-        navigation={true}
         thumbs={{
           swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
         }}
